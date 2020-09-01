0a7719566c4a0487ac1e8113fa74091793f7d642
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-01 02:59:31 +0000

    Added get request for /urls/new

--- express_server.js
+++ express_server.js
@@ -9,6 +9,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
