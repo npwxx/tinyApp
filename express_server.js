f90b380b4ed6f36646cb8300395510608f5ac49d
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-01 01:36:35 +0000

    Added headers to urls_index & urls_show

--- express_server.js
+++ express_server.js
@@ -15,7 +15,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const { shortURL } = req.params;
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
 });
 
