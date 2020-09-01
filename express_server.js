f2bff3bd4a07c798c57eafe79c0cd5714b2abdb7
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-01 23:19:10 +0000

    Added delete & partial edit

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -12,6 +13,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -45,6 +51,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -52,7 +63,7 @@ app.listen(PORT, () => {
 
 const generateRandomString = function() {
   let result = '';
-  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
   for (let i = 0; i < 6; i++) {
     result += characters[Math.floor(Math.random() * charactersLength)];
