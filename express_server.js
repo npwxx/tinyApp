84bcda018c478650f7a275306849707e5b1a5f45
Nicole Woodcock <nicole.xox@gmail.com>
2020-09-02 03:56:31 +0000

    added login/logout functionality using cookies

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -15,26 +17,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-//
+//redirects to index page.
 app.get("/", (req, res) => {
   res.redirect("/urls");
 });
 
 //Brings you to the page to create a new URL
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: getUsername(req) };
+  res.render("urls_new", templateVars);
+  console.log(getUsername(req));
 });
 
-//
+//renders the index page
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: getUsername(req) };
   res.render("urls_index", templateVars);
 });
 
-//
+//renders a page where you can edit the URL
 app.get("/urls/:shortURL", (req, res) => {
   const { shortURL } = req.params;
-  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  let templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL], username: getUsername(req) };
   res.render("urls_show", templateVars);
 });
 
@@ -46,13 +50,13 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
-//redirects
+//redirects to long URL
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//
+//shows the database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -71,6 +75,20 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+//set a cookie &  log in.
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  console.log(username);
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+//logout deletes the username cookie
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 //opens a port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -85,4 +103,9 @@ const generateRandomString = function() {
     result += characters[Math.floor(Math.random() * charactersLength)];
   }
   return result;
+};
+
+//Funtion to check if there is already username Cookies
+const getUsername = function(req) {
+  return req.cookies ? req.cookies['username'] : undefined;
 };
